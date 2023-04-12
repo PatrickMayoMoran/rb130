a6654e7e64e620689e1c4a068609eefc020eb69a
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-12 22:49:00 +0000

    Add more examples to assignment 4

--- lesson_1/assignment_4.rb
+++ lesson_1/assignment_4.rb
@@ -1,4 +1,10 @@
-def echo_with_block(str)
+def echo_with_yield(str)
   yield
   str
 end
+
+# echo_with_yield { p "this!" } - ArgumentError; given 0, expected 1
+echo_with_yield("meow") { p "this!" }
+# echo_with_yield("meow", "cat") { p "this!" } - ArgumentError; 2 for 1
+
+# echo_with_yield("meow") - LocalJumpError; no block given (yield)
