d9a30264c954a7bc6f78b4d29e694f9978403d50
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-13 14:25:50 +0000

    Add usage of block_given? to method with yield

--- lesson_1/assignment_4.rb
+++ lesson_1/assignment_4.rb
@@ -1,10 +1,12 @@
 def echo_with_yield(str)
-  yield
+  yield if block_given?
   str
 end
 
 # echo_with_yield { p "this!" } - ArgumentError; given 0, expected 1
-echo_with_yield("meow") { p "this!" }
+p echo_with_yield("meow") { p "this!" }
 # echo_with_yield("meow", "cat") { p "this!" } - ArgumentError; 2 for 1
 
-# echo_with_yield("meow") - LocalJumpError; no block given (yield)
+p echo_with_yield("meow") # - LocalJumpError; no block given (yield)
+# Adding block_given? means we can add in functionality of still running
+# the method when a block has not been given
