561313cfb81c47fb2b990655c20f22f1f7c130b9
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-13 14:15:19 +0000

    Add PEDAC for own implementation of times method

---
+++ lesson_1/own_times.rb
@@ -0,0 +1,19 @@
+# Write own implementation of the times method
+#
+# INPUT: self and block
+# OUTPUT: self
+#
+# Q's: 
+#   How to implement 'self' with method defined outside of a class?
+#   Mimic by passing integer as argument?
+#
+# Algorithm:
+#   counter starts at 0
+#   while counter is less than given number,
+#     yield counter to the block
+#     increment counter by 1
+#
+#   return given number
+
+def times(n)
+end
