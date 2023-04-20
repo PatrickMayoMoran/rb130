69feeea345ebcb38c25e285729c93b3cfc4491ab
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-20 13:23:30 +0000

    Add solution using parameter flag and ternary operator instead of long if

--- lesson_1/take_2/own_reduce.rb
+++ lesson_1/take_2/own_reduce.rb
@@ -1,21 +1,36 @@
-def reduce(array, start=nil)
-  counter = 0
-  acc = nil
+# OPTION 1 - own solution that handles edge cases with long if statement
+# def reduce(array, start=nil)
+#   counter = 0
+#   acc = nil
+# 
+#   if start
+#     acc = start
+#     while counter < array.size
+#       acc = yield(acc, array[counter])
+#       counter += 1
+#     end
+#   else
+#     acc = array[counter]
+#     counter += 1
+#     loop do
+#       break if counter >= array.size
+#       acc = yield(acc, array[counter])
+#       counter += 1
+#     end
+#   end
+# 
+#   acc
+# end
+#
+# OPTION 2 - use of ternary and parameter flag
+def reduce(collection, start = (not_given = true))
+  return collection if collection.empty?
+  acc = not_given ? collection[0] : start
+  counter = not_given ? 1 : 0
 
-  if start
-    acc = start
-    while counter < array.size
-      acc = yield(acc, array[counter])
-      counter += 1
-    end
-  else
-    acc = array[counter]
+  while counter < collection.size
+    acc = yield(acc, collection[counter])
     counter += 1
-    loop do
-      break if counter >= array.size
-      acc = yield(acc, array[counter])
-      counter += 1
-    end
   end
 
   acc
@@ -23,6 +38,7 @@ end
 
 array = [1, 2, 3, 4, 5]
 
+p reduce([])
 p reduce(array) { |acc, num| acc + num }                    # => 15
 p reduce(array, 10) { |acc, num| acc + num }                # => 25
 # p reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
