1aad54a4a8873f2f599b1d3f25cda40fb2b6e3de
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-20 15:15:49 +0000

    Add example of explicit ampersand being able to yield and call

--- practice/ampersand_explicit_test.rb
+++ practice/ampersand_explicit_test.rb
@@ -1,7 +1,9 @@
 def ampersand_test(&prock)
+  yield
   prock.call
 end
 
 p = proc { puts "meow" }
 
-ampersand_test(p) # Doesn't work - ampersand means expecting explicit block
+# ampersand_test(p) # Doesn't work - ampersand means expecting explicit block
+ampersand_test { puts "meow" }
