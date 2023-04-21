2ee9a5a7b075fee3ee3ef6357a9962b4e06041f1
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-21 12:12:32 +0000

    Add example of block/proc back and forth conversion

--- practice/ampersand_explicit_test.rb
+++ practice/ampersand_explicit_test.rb
@@ -1,9 +1,26 @@
 def ampersand_test(&prock)
+  p prock
   yield
   prock.call
+  test_2(prock)
 end
+
+def test_2(prock)
+  p "------"
+  prock.call
+  # yield - doesn't work here
+  test_3(&prock)
+end
+
+def test_3(&prock)
+  p "----"
+  prock.call
+  yield # both work again!
+end
+
+ampersand_test { puts "meow" }
 
 p = proc { puts "meow" }
 
 # ampersand_test(p) # Doesn't work - ampersand means expecting explicit block
-ampersand_test { puts "meow" }
+
