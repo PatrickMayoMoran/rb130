f4d53b73100f4dc614ea52e541ea7c303528b529
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-25 22:55:01 +0000

    Add own solution to each consecutive exercise

---
+++ exercises/easy_2/each_cons_1.rb
@@ -0,0 +1,32 @@
+def each_cons(arr)
+  return nil if arr.size <= 1
+  i1 = 0
+  i2 = 1
+  while i2 < arr.size
+    yield(arr[i1], arr[i2])
+    i1 += 1
+    i2 += 1
+  end
+  nil
+end
+
+hash = {}
+result = each_cons([1, 3, 6, 10]) do |value1, value2|
+  hash[value1] = value2
+end
+p result == nil
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+result = each_cons([]) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {}
+p result == nil
+
+hash = {}
+result = each_cons(['a', 'b']) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {'a' => 'b'}
+p result == nil
