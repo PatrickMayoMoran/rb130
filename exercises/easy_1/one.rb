3028f721e6a596bdba766fa834cb29f794bab54b
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-25 16:56:40 +0000

    Add own implementation of Enumerable one?

---
+++ exercises/easy_1/one.rb
@@ -0,0 +1,19 @@
+def one?(array)
+  truthy = 0
+
+  array.each do |el|
+    truthy += 1 if yield(el)
+    return false if truthy > 1
+  end
+
+  return true if truthy == 1
+  false
+end
+
+p one?([1, 3, 5, 6]) { |value| value.even? }    # -> true
+p one?([1, 3, 5, 7]) { |value| value.odd? }     # -> false
+p one?([2, 4, 6, 8]) { |value| value.even? }    # -> false
+p one?([1, 3, 5, 7]) { |value| value % 5 == 0 } # -> true
+p one?([1, 3, 5, 7]) { |value| true }           # -> false
+p one?([1, 3, 5, 7]) { |value| false }          # -> false
+p one?([]) { |value| true }                     # -> false
