ec0f508407126e411849850b2147e51405216277
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-25 23:09:00 +0000

    Add solution to each_cons 2 exercise

---
+++ exercises/easy_2/each_cons_2.rb
@@ -0,0 +1,38 @@
+def each_cons(arr, block_args)
+  arr.each_with_index do |el, i|
+    break if block_args > arr.size
+    break if i > (arr.size - block_args)
+    yield(el, *arr[i+1, (block_args-1)])
+  end
+end
+
+hash = {}
+each_cons([1, 3, 6, 10], 1) do |value|
+  hash[value] = true
+end
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+
+hash = {}
+each_cons([1, 3, 6, 10], 2) do |value1, value2|
+  hash[value1] = value2
+end
+p hash
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+each_cons([1, 3, 6, 10], 3) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6], 3 => [6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 4) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 5) do |value1, *values|
+  hash[value1] = values
+end
+p hash == {}
