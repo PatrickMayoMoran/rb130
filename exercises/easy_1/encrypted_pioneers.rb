9ad80cbba135c5340872d70a9b5a50ca9104b63f
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-25 14:38:06 +0000

    Add prompt for encrypted pioneers problem

---
+++ exercises/easy_1/encrypted_pioneers.rb
@@ -0,0 +1,22 @@
+def rot_13(str)
+end
+
+p rot_13 'Nqn Ybirynpr'
+p rot_13 'Tenpr Ubccre'
+p rot_13 'Nqryr Tbyqfgvar'
+p rot_13 'Nyna Ghevat'
+p rot_13 'Puneyrf Onoontr'
+p rot_13 'Noqhyynu Zhunzznq ova Zhfn ny-Xujnevmzv'
+p rot_13 'Wbua Ngnanfbss'
+p rot_13 'Ybvf Unvog'
+p rot_13 'Pynhqr Funaaba'
+p rot_13 'Fgrir Wbof'
+p rot_13 'Ovyy Tngrf'
+p rot_13 'Gvz Orearef-Yrr'
+p rot_13 'Fgrir Jbmavnx'
+p rot_13 'Xbaenq Mhfr'
+p rot_13 'Fve Nagbal Ubner'
+p rot_13 'Zneiva Zvafxl'
+p rot_13 'Lhxvuveb Zngfhzbgb'
+p rot_13 'Unllvz Fybavzfxv'
+p rot_13 'Tregehqr Oynapu'
