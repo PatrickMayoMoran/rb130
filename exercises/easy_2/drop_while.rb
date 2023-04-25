5d52654d68abae5f86973f48a3d9c4d9db1a8967
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-25 22:23:52 +0000

    Add solution to own implementation of drop_while method

---
+++ exercises/easy_2/drop_while.rb
@@ -0,0 +1,15 @@
+def drop_while(arr)
+  index = 0
+  while index < arr.size
+    break unless yield(arr[index])
+    index += 1
+  end
+  arr[index..]
+end
+
+p drop_while([1, 3, 5, 6]) { |value| value.odd? } == [6]
+p drop_while([1, 3, 5, 6]) { |value| value.even? } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| true } == []
+p drop_while([1, 3, 5, 6]) { |value| false } == [1, 3, 5, 6]
+p drop_while([1, 3, 5, 6]) { |value| value < 5 } == [5, 6]
+p drop_while([]) { |value| true } == []
