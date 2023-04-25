140863e05f55a3b8a007eab0c82b4c153bf9341f
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-25 10:19:17 +0000

    Add solution to optional blocks extension exercise

--- exercises/easy_1/optional_blocks.rb
+++ exercises/easy_1/optional_blocks.rb
@@ -1,6 +1,6 @@
 # Option 1 - explicit return of block return value if block given
-def compute
-  return yield if block_given?
+def compute(arg)
+  return yield(arg) if block_given?
   "Does not compute."
 end
 
@@ -13,9 +13,13 @@ def compute2
   end
 end
 
-p compute { 5 + 3 } == 8
-p compute { 'a' + 'b' } == 'ab'
-p compute == 'Does not compute.'
-p compute2 { 5 + 3 } == 8
-p compute2 { 'a' + 'b' } == 'ab'
-p compute2 == 'Does not compute.'
+p compute(5) { |n| n + 7 }
+p compute(5)
+p compute("meow") { |s| s + " said the cat" }
+
+# p compute { 5 + 3 } == 8
+# p compute { 'a' + 'b' } == 'ab'
+# p compute == 'Does not compute.'
+# p compute2 { 5 + 3 } == 8
+# p compute2 { 'a' + 'b' } == 'ab'
+# p compute2 == 'Does not compute.'
