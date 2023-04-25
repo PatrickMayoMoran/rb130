f5022989ee4cfd77fe7c64bafffa0d9a41680fa8
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-25 22:03:20 +0000

    Add solution to count exercise

---
+++ exercises/easy_2/count.rb
@@ -0,0 +1,25 @@
+def count(array)
+  count = 0
+  array.each { |el|count += 1 if yield(el) }
+  count
+end
+#
+# OPTION 2 - no each
+# def count(array)
+#   iterator = 0
+#   count = 0
+# 
+#   while iterator < array.size
+#     count += 1 if yield(array[iterator])
+#     iterator += 1
+#   end
+# 
+#   count
+# end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
