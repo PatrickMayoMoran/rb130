50db5b0a26ef3b1fb75b4009bde54abac5db031f
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 14:41:13 +0000

    Add alternative text_analyzer implementation using readlines

--- exercises/medium_1/text_analyzer.rb
+++ exercises/medium_1/text_analyzer.rb
@@ -1,12 +1,18 @@
 class TextAnalyzer
   def process
     f = File.open('rb130_review.txt', 'r')
-    yield(f.read)
+    yield(f)
     f.close
   end
 end
 
+# Original solution
 analyzer = TextAnalyzer.new
-analyzer.process { |f| puts "#{f.split(/\n\n/).count} paragraphs" }
-analyzer.process { |f| puts "#{f.split(/\n/).count} lines" }
-analyzer.process { |f| puts "#{f.split(/ /).count} words" }
+analyzer.process { |f| puts "#{f.read.split(/\n\n/).count} paragraphs" }
+analyzer.process { |f| puts "#{f.read.split(/\n/).count} lines" }
+analyzer.process { |f| puts "#{f.read.split(/ /).count} words" }
+
+# Extra solution with readlines for practice
+analyzer.process { |f| puts "#{f.readlines('').count} paragraphs" }
+analyzer.process { |f| puts "#{f.readlines.count} lines" }
+analyzer.process { |f| puts "#{f.readlines(' ').count} words" }
