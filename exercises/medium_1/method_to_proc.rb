7d106e3c24f03998503365c59ee9aec401132128
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 12:21:26 +0000

    Replace placeholders with needed method and proc

--- exercises/medium_1/method_to_proc.rb
+++ exercises/medium_1/method_to_proc.rb
@@ -5,8 +5,8 @@ end
 
 # Replace `argument` with the correct argument below
 # `method` is `Object#method`, not a placeholder
-base8_proc = method(argument).to_proc
+base8_proc = method(convert_to_base_8).to_proc
 
 # We'll need a Proc object to make this code work
 # Replace `a_proc` with the correct object
-[8, 10, 12, 14, 16, 33].map(&a_proc)
+[8, 10, 12, 14, 16, 33].map(&base8_proc)
