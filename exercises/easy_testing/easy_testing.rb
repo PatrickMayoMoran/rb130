6de408a660dcc211eaec60024d7016cfacd3c2db
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 10:39:01 +0000

    Add equality assertion

--- exercises/easy_testing/easy_testing.rb
+++ exercises/easy_testing/easy_testing.rb
@@ -3,9 +3,12 @@ require 'minitest/autorun'
 class Exercise < Minitest::Test
 
   def test_odd
-    value = 2
+    value = 1
     assert(value.odd?)
   end
 
-
+  def test_equality
+    value = 'XYZ'
+    assert_equal('xyz', value.downcase)
+  end
 end
