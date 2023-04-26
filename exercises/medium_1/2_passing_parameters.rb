bc17a48c957bec6891e7a73cd22748feb42211d7
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 13:23:23 +0000

    Add provided solution to second passing parameters exercise

--- exercises/medium_1/2_passing_parameters.rb
+++ exercises/medium_1/2_passing_parameters.rb
@@ -2,10 +2,18 @@ def raptor_array(arr)
   yield arr
 end
 
+def birdies(birds)
+  yield birds
+end
+
+# Provided solution
+birdies(['Raven', 'Finch', 'Hawk', 'Eagle']) do |_, _, *raptors|
+  puts "Rapters: #{raptors.join(' --- ')}"
+end
+
+# Original solution below
 raptors = nil
 
 raptor_array(['Raven', 'Finch', 'Hawk', 'Eagle']) do |rav, fin, *rap|
   raptors = rap
 end
-
-p raptors
