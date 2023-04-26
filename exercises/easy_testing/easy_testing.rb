dd8bdde3597086206e98e88705ed125d1b2abc85
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 10:40:19 +0000

    Add optional error messages to tests

--- exercises/easy_testing/easy_testing.rb
+++ exercises/easy_testing/easy_testing.rb
@@ -3,12 +3,12 @@ require 'minitest/autorun'
 class Exercise < Minitest::Test
 
   def test_odd
-    value = 1
-    assert(value.odd?)
+    value = 2
+    assert(value.odd?, "Value not odd")
   end
 
   def test_equality
-    value = 'XYZ'
-    assert_equal('xyz', value.downcase)
+    value = 'BYZ'
+    assert_equal('xyz', value.downcase, "Downcase won't save you!")
   end
 end
