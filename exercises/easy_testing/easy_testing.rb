08057cfc706e3032a6bc77bd64a0406469f08f62
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 10:44:10 +0000

    Change tests to pass to avoid cluttered output

--- exercises/easy_testing/easy_testing.rb
+++ exercises/easy_testing/easy_testing.rb
@@ -3,12 +3,12 @@ require 'minitest/autorun'
 class Exercise < Minitest::Test
 
   def test_odd
-    value = 2
+    value = 1
     assert(value.odd?, "Value not odd")
   end
 
   def test_equality
-    value = 'BYZ'
+    value = 'XYZ'
     assert_equal('xyz', value.downcase, "Downcase won't save you!")
   end
 
