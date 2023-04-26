7ed2a275ef0867b8ec4e33cb75d35bf539e36556
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 23:53:27 +0000

    Add skeleton test for accept money

--- exercises/medium_testing/test_cash_register.rb
+++ exercises/medium_testing/test_cash_register.rb
@@ -4,4 +4,12 @@ require_relative 'cash_register'
 require_relative 'transaction'
 
 class CashRegisterTest < Minitest::Test
+  def setup
+    @cash_register = CashRegister.new(0)
+  end
+
+  def test_accept_money
+    @cash_register.accept_money(100)
+
+  end
 end
