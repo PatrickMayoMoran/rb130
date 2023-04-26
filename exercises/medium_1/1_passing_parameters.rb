8cdf44b0c719817bd83ef5f20cd0971cfe28a842
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-26 12:54:15 +0000

    Add solution to passing parameters 1 exercise

--- exercises/medium_1/1_passing_parameters.rb
+++ exercises/medium_1/1_passing_parameters.rb
@@ -2,6 +2,8 @@ items = ['apples', 'corn', 'cabbage', 'wheat']
 
 def gather(items)
   puts "Let's start gathering food."
-  puts "#{items.join(', ')}"
+  yield(items)
   puts "Nice selection of food we have gathered!"
 end
+
+gather(items) { |i| puts "We've got #{i.join('-')}!" }
