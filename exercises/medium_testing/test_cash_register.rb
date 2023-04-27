a1fd1e8268a8894d5aca0d5f1a99ca24f171225e
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-27 10:57:40 +0000

    Add setup and accept money test

--- exercises/medium_testing/test_cash_register.rb
+++ exercises/medium_testing/test_cash_register.rb
@@ -5,11 +5,13 @@ require_relative 'transaction'
 
 class CashRegisterTest < Minitest::Test
   def setup
-    @cash_register = CashRegister.new(0)
+    @cash_register = CashRegister.new(1000)
+    @transaction = Transaction.new(50)
+    @transaction.amount_paid = 50
   end
 
   def test_accept_money
-    @cash_register.accept_money(100)
-
+    @cash_register.accept_money(@transaction)
+    assert_equal(1050, @cash_register.total_money)
   end
 end
