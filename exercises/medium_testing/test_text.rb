120d52c8cabdde5867aa95ae9132d15b53748674
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-27 15:27:19 +0000

    Add test for word_count

--- exercises/medium_testing/test_text.rb
+++ exercises/medium_testing/test_text.rb
@@ -5,11 +5,11 @@ require_relative 'text'
 class TextTest < Minitest::Test
   def setup
     @sample = File.open('text.txt', 'r')
-    @text_object = Text.new(@sample.read)
+    @text = Text.new(@sample.read)
   end
 
   def test_swap
-    actual = @text_object.swap('a', 'e')
+    actual = @text.swap('a', 'e')
     expected = <<~HEREDOC
       Lorem ipsum dolor sit emet, consectetur edipiscing elit. Cres sed vulputete ipsum.
       Suspendisse commodo sem ercu. Donec e nisi elit. Nullem eget nisi commodo, volutpet
@@ -18,6 +18,20 @@ class TextTest < Minitest::Test
       dolor ornere bibendum. Morbi ut messe nec lorem tincidunt elementum vitee id megne. Cres
       et verius meuris, et pheretre mi.
     HEREDOC
+    assert_equal(expected, actual)
+  end
+
+  def test_count
+    actual = @text.word_count
+    text = <<~HEREDOC
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed vulputate ipsum.
+Suspendisse commodo sem arcu. Donec a nisi elit. Nullam eget nisi commodo, volutpat
+quam a, viverra mauris. Nunc viverra sed massa a condimentum. Suspendisse ornare justo
+nulla, sit amet mollis eros sollicitudin et. Etiam maximus molestie eros, sit amet dictum
+dolor ornare bibendum. Morbi ut massa nec lorem tincidunt elementum vitae id magna. Cras
+et varius mauris, at pharetra mi.
+    HEREDOC
+    expected = text.split.count
     assert_equal(expected, actual)
   end
 
