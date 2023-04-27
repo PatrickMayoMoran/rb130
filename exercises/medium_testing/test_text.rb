1860d40ccc81deba9a10b8361e110c6560527b7a
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-27 15:20:43 +0000

    Add provided solution

--- exercises/medium_testing/test_text.rb
+++ exercises/medium_testing/test_text.rb
@@ -6,15 +6,22 @@ class TextTest < Minitest::Test
   def setup
     @sample = File.open('text.txt', 'r')
     @text_object = Text.new(@sample.read)
-    @sample.rewind
   end
 
   def test_swap
-    assert_equal(@sample.read.gsub('a', 'e'), @text_object.swap('a', 'e'))
+    actual = @text_object.swap('a', 'e')
+    expected = <<~HEREDOC
+      Lorem ipsum dolor sit emet, consectetur edipiscing elit. Cres sed vulputete ipsum.
+      Suspendisse commodo sem ercu. Donec e nisi elit. Nullem eget nisi commodo, volutpet
+      quem e, viverre meuris. Nunc viverre sed messe e condimentum. Suspendisse ornere justo
+      nulle, sit emet mollis eros sollicitudin et. Etiem meximus molestie eros, sit emet dictum
+      dolor ornere bibendum. Morbi ut messe nec lorem tincidunt elementum vitee id megne. Cres
+      et verius meuris, et pheretre mi.
+    HEREDOC
+    assert_equal(expected, actual)
   end
 
   def teardown
-    @sample.rewind
     @sample.close
   end
 end
