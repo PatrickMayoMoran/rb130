de65198b63956bf84b4631ca7b7d66fca000afd0
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-27 11:11:38 +0000

    Add test for giving receipt

--- exercises/medium_testing/test_cash_register.rb
+++ exercises/medium_testing/test_cash_register.rb
@@ -7,11 +7,21 @@ class CashRegisterTest < Minitest::Test
   def setup
     @cash_register = CashRegister.new(1000)
     @transaction = Transaction.new(50)
-    @transaction.amount_paid = 50
+    @transaction.amount_paid = 80
   end
 
   def test_accept_money
     @cash_register.accept_money(@transaction)
-    assert_equal(1050, @cash_register.total_money)
+    assert_equal(1080, @cash_register.total_money)
+  end
+
+  def test_change
+    change = @cash_register.change(@transaction)
+    assert_equal(30, change)
+  end
+
+  def test_give_receipt
+    expected = "You've paid $50.\n"
+    assert_output(expected) { @cash_register.give_receipt(@transaction) }
   end
 end
