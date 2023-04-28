25dc2c60db322abefdf9e81e3f703bef1993aa71
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-28 15:51:57 +0000

    Remove PEDAC and unused class constant

--- challenges/roman_numerals/roman_numerals.rb
+++ challenges/roman_numerals/roman_numerals.rb
@@ -1,40 +1,4 @@
-=begin
-GOAL: convert a base 10 integer its Roman equivalent as a string
-
-INPUT: Integer
-OUTPUT: string
-
-RULES
-  numbers can't be above 3000
-  if digit is one, two or three, write more of that digit
-  if digit is four, write one unit before 5 unit
-  if digit is five, six, seven, or eight, write 5 plus ones
-  if digit is nine, write next 10 unit minus one
-
-DATA STRUCTURES
-
-ALGORITHM
-  count digits of the number to know how many places we start at
-  number of times we'll iterate is number of digits
-
-  ### Current divisor for 1000 is 1 - iteration number
-  1 -> 1000 % 10*3
-  2 -> 1000 % 10*2
-  3 -> 1000 % 10*1
-  4 -> 1000 % 0
-
-CODE
-  single digit number
-    return '' if number is 0
-    return 'MARKER' * number if number is 1, 2, 3
-    return 'MARKER' + '5_MARKER' if number is 4
-    return '5_MARKER' if number is 5
-    return '5_MARKER' + 'MARKER' * number % 5 if number is 6, 7, 8
-    return 'MARKER' + '10_MARKER' if number is 9
-=end
-
 class RomanNumeral
-
   def initialize(number)
     self.number = number
   end
