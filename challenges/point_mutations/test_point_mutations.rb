70e858babdc100d49bab8859d2b09f799cabcc7c
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-28 01:15:24 +0000

    Remove skips to show all passing tests

--- challenges/point_mutations/test_point_mutations.rb
+++ challenges/point_mutations/test_point_mutations.rb
@@ -3,17 +3,14 @@ require_relative 'point_mutations'
 
 class DNATest < Minitest::Test
   def test_no_difference_between_empty_strands
-    skip
     assert_equal 0, DNA.new('').hamming_distance('')
   end
 
   def test_no_difference_between_identical_strands
-    skip
     assert_equal 0, DNA.new('GGACTGA').hamming_distance('GGACTGA')
   end
 
   def test_complete_hamming_distance_in_small_strand
-    skip
     assert_equal 3, DNA.new('ACT').hamming_distance('GGA')
   end
 
@@ -24,22 +21,18 @@ class DNATest < Minitest::Test
   end
 
   def test_small_hamming_distance_in_middle_somewhere
-    skip
     assert_equal 1, DNA.new('GGACG').hamming_distance('GGTCG')
   end
 
   def test_larger_distance
-    skip
     assert_equal 2, DNA.new('ACCAGGG').hamming_distance('ACTATGG')
   end
 
   def test_ignores_extra_length_on_other_strand_when_longer
-    skip
     assert_equal 3, DNA.new('AAACTAGGGG').hamming_distance('AGGCTAGCGGTAGGAC')
   end
 
   def test_ignores_extra_length_on_original_strand_when_longer
-    skip
     strand = 'GACTACGGACAGGGTAGGGAAT'
     distance = 'GACATCGCACACC'
     assert_equal 5, DNA.new(strand).hamming_distance(distance)
