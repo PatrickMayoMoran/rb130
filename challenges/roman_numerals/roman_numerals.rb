f0f552bd6f87d309f3e3904a4fd0e91e47f21430
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-28 09:47:43 +0000

    Add hash with numeral values

--- challenges/roman_numerals/roman_numerals.rb
+++ challenges/roman_numerals/roman_numerals.rb
@@ -1,9 +1,20 @@
 class RomanNumeral
+  NUMERALS = {
+    'I' => 1,
+    'V' => 5,
+    'X' => 10,
+    'L' => 50,
+    'C' => 100,
+    'D' => 500,
+    'M' => 1000
+  }
+
   def initialize(number)
     self.number = number
   end
 
   def to_roman
+
   end
 
   private
