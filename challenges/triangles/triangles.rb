f1e927398c83e2962b0f73e3dfc93866fbcfcd6a
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-04-28 00:35:29 +0000

    Correct rubocop unless indentation complaint

--- challenges/triangles/triangles.rb
+++ challenges/triangles/triangles.rb
@@ -32,7 +32,7 @@ class Triangle
 
   def valid_triangle?(side1, side2, side3)
     raise ArgumentError unless (side1 + side2 > side3) &&
-    (side2 + side3 > side1) &&
-    (side1 + side3 > side2)
+                               (side2 + side3 > side1) &&
+                               (side1 + side3 > side2)
   end
 end
