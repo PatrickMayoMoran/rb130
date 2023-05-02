00e748483706c15b1db46e3434f59bf12bce15fc
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-05-02 14:17:09 +0000

    Add creation of array with all needed letters

--- challenges/medium/diamond/diamond.rb
+++ challenges/medium/diamond/diamond.rb
@@ -1,2 +1,19 @@
+# Goal: produce a diamond with all the letters up to given letter
+#
+# Rules:
+#   given letter only appears once
+#   other letters appear twice
+#   given letter is center
+#   all other lines are that same width
+#   so diamond is a square
+#   a returns by itself
+#
+# ALGORITHM
+#   Build an array of all the letters we need
+#   range from 'A' to given letter converted to Array PLUS range 
+#   from 'A' to given letter non-inclusive, reversed
 class Diamond
+  def self.make_diamond(letter)
+    letters = ('A'..letter).to_a + ('A'...letter).to_a.reverse
+  end
 end
