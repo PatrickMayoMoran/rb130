0d0369a3326457e66892fb45278f6b6d8c5f5c55
Patrick Moran <patrick.mayo.moran@gmail.com>
2023-05-02 15:16:12 +0000

    Add at class method which creates a new clock instance

--- challenges/medium/clock/clock.rb
+++ challenges/medium/clock/clock.rb
@@ -1,11 +1,21 @@
 class Clock
-  attr_reader :time
+  attr_reader :time, :hours, :minutes
 
   def initialize(hours, minutes=0)
     @time = hours * 60 + minutes
+    @hours = hours
+    @minutes = minutes
+  end
+
+  def self.at(hours, minutes=0)
+    Clock.new(hours, minutes)
   end
 
   def ==(other)
     time == other.time
+  end
+
+  def to_s
+    sprintf('%02d:%02d', hours, minutes)
   end
 end
